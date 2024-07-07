c5ddc2486edb9e3a0769bccdf687daeb24d7f8c8
nogula <noahrgula@gmail.com>
2024-07-07 17:05:20 -0500

    black

--- FigureForge/main.py
+++ FigureForge/main.py
@@ -2,11 +2,13 @@ from PySide6.QtWidgets import QApplication
 
 from .gui import MainWindow
 
+
 def main():
     app = QApplication([])
     window = MainWindow()
     window.show()
     app.exec()
+
 
 if __name__ == "__main__":
     app = QApplication([])
