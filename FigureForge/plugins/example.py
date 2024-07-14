3de97fe32fe04c3a316bd370caa17f3778c86770
nogula <noahrgula@gmail.com>
2024-07-14 15:09:39 -0500

    Refactored item to obj

--- FigureForge/plugins/example.py
+++ FigureForge/plugins/example.py
@@ -11,34 +11,34 @@ from .utils.select_spines_dialog import SelectSpinesDialog
 
 class ExamplePlugin:
     name = "Remove Spines"
-    tooltip = "Remove spines from the selected item."
+    tooltip = "Remove spines from the selected obj."
     icon = os.path.join(os.path.dirname(__file__), "remove_spines.png")
     submenu = "Spines"
 
-    def run(self, item):
+    def run(self, obj):
         msg_success = QMessageBox()
         msg_success.setText("Spines removed successfully.")
         msg_success.setIcon(QMessageBox.Information)
-        if isinstance(item, Spine):
-            item.set_visible(False)
+        if isinstance(obj, Spine):
+            obj.set_visible(False)
             msg_success.exec()
 
-        elif isinstance(item, (Figure, Axes)):
+        elif isinstance(obj, (Figure, Axes)):
             dialog = SelectSpinesDialog()
             if dialog.exec() == QDialog.Accepted:
                 selected_spines = dialog.get_selected_spines()
-                if isinstance(item, Figure):
-                    for ax in item.axes:
+                if isinstance(obj, Figure):
+                    for ax in obj.axes:
                         for spine, remove in selected_spines.items():
                             if remove:
                                 ax.spines[spine].set_visible(False)
-                elif isinstance(item, Axes):
+                elif isinstance(obj, Axes):
                     for spine, remove in selected_spines.items():
                         if remove:
-                            item.spines[spine].set_visible(False)
+                            obj.spines[spine].set_visible(False)
                 msg_success.exec()
         else:
             msg_failure = QMessageBox()
-            msg_failure.setText(f"Invalid item type: {type(item)}.")
+            msg_failure.setText(f"Invalid obj type: {type(obj)}.")
             msg_failure.setIcon(QMessageBox.Warning)
             msg_failure.exec()
